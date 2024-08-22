884dca32b878f621ca3e54276de41384b5dac6bb
Caliese <135731149+Caliese@users.noreply.github.com>
2024-08-22 16:25:16 -0400

    Add files via upload

---
+++ AUG-22.R
@@ -0,0 +1,18 @@
+#Author: Caliese Beckford, Date: 08/22/2024, Purpose: Test the measures of descriptive stats
+
+#How to calculate standard deviation in R
+X <- c(1,2,3,4,5)
+sd(X)
+
+#Use 'c' and brackets (container) when there are more than one numbers
+#Y <- 100
+#same format as above to append to  X
+#to see what is stored in variable just run the name of the variable. eg > X
+
+#How to calculate mean in R
+mean(X)
+
+#How to calculate median in R
+median(X)
+
+#there is no function for mode in R
